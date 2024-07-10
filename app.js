a7e89b5aa4933935feb7e7b43508d9ebc40e0023
Zenox2003 <smith.lindley93313@gmail.com>
2024-07-10 16:14:27 -0400

    Exercise comments

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 console.log("We doing boys!");
+
+// Exercise 1
+
+// Exercsie 2
+
+// Exercise 3
+
+// Exercise 4
+
+// Exercise 5
+
+// Exercise 6
+
+// Exercise 7
+
+// Exercise 8 
+
+// Exercise 9
+
+// Exercise 10
+
+// Exercise 11
+
+// Exercise 12
+
+// Exercise 13
+
+// Exercise 14
+
+// Exercise 15
+
